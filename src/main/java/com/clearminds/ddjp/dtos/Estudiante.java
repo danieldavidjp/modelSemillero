5eb7fe7e70dac382804fead3a7981973d15b71ed
Daniel Jimenez <danieldavidjp@gmail.com>
2020-12-10 09:57:49 -0500

    Se anade a la clase estudiante un integer para la edad, se crean sus respectivo getter y setter, se anaden constructores, y se le da un condicional al get de edad para que retorne null

--- src/main/java/com/clearminds/ddjp/dtos/Estudiante.java
+++ src/main/java/com/clearminds/ddjp/dtos/Estudiante.java
@@ -1,52 +1,86 @@
 package com.clearminds.ddjp.dtos;
 
 public class Estudiante {
-	
+
 	private String nombre;
 	private String apellido;
 	private int id;
-	
-	//Constructor con parametros
-	public Estudiante(String nombre, String apellido, int id) {
-		super();
+	private Integer edad;
+
+	// Constructor con parametros
+	public Estudiante(int id, String nombre, String apellido, int edad) {
+
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.id = id;
+		this.edad = edad;
+	}
+	// Constructor con parametros sin id
+
+	public Estudiante(String nombre, String apellido) {
+
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+
+	public Estudiante(String nombre, String apellido, int edad) {
+
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+	}
+
+	public Estudiante(int id, String nombre, String apellido) {
+
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.id = id;
 	}
-	//Constructor con parametros sin id
-	
-	public Estudiante(String nombre, String apellido) {
-		super();
-		this.nombre = nombre;
-		this.apellido = apellido;
-	}
-	//Metodo toString
 
+	// Metodo toString
 	@Override
+
 	public String toString() {
-		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + "]";
+		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + ", edad=" + edad + "]";
 	}
 
 	// Getters and Setters
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	
-	
+
+	public Integer getEdad() {
+		if (edad == null || edad == 0)
+			return null;
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public Estudiante() {
+
+	}
 
 }
